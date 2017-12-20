c6339e87cf4f5411cddac2cbdeeffccc0472b84d
Emilia <emciastek@gmail.com>
2017-12-20 14:08:58 +0100

    delete unsed methods and classes

--- server/src/test/java/fleet/CustomFleetTest.java
+++ server/src/test/java/fleet/CustomFleetTest.java
@@ -1,83 +1,24 @@
 package fleet;
 
-import model.ShipModel;
-import org.testng.annotations.DataProvider;
+import model.FleetModel;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.testng.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class CustomFleetTest {
 
     @Test
-    public void givenEmptyFleetWhenGetFleetSizeThenReturn0() {
-        CustomFleet fleet = new CustomFleet();
-        int expectedFleetSize = 0;
+    public void givenPositionWhenHitThenPositionAddedToHitFields() {
+        FleetModel fleetModel = mock(FleetModel.class);
+        CustomFleet fleet = new CustomFleet(fleetModel);
+        int hitPosition = 1;
 
-        assertEquals(fleet.getSize(), expectedFleetSize);
-    }
+        fleet.hit(hitPosition);
+        List<Integer> actualHits = fleet.getHitFields();
 
-    @Test
-    public void whenAddShipAndGetFleetSizeThenSizeIsIncremented() {
-        CustomFleet fleet = new CustomFleet();
-        int initialFleetSize = fleet.getSize();
-        int expectedFleetSize = initialFleetSize + 1;
-        ShipModel ship = new ShipModel(Arrays.asList(0));
-
-        fleet.addShip(ship);
-
-        assertEquals(fleet.getSize(), expectedFleetSize);
-    }
-
-    @DataProvider(name = "ships to be added to fleet")
-    private Object[] shipsToBeAdded() {
-        return new Object[]{
-                generateOneMastShip(),
-                generateTwoMastShip(),
-                generateOneMastShip()
-        };
-    }
-
-    @Test(dataProvider = "ships to be added to fleet")
-    public void whenAddShipsAndGetShipsThenReturnShips(ShipModel[] ships) {
-        CustomFleet fleet = new CustomFleet();
-
-        for (ShipModel ship : ships) {
-            fleet.addShip(ship);
-        }
-
-        List<ShipModel> actualShips = fleet.getShips();
-        int fleetSize = fleet.getSize();
-
-        assertThat(fleetSize).isEqualTo(ships.length);
-        assertThat(actualShips).containsExactly(ships);
-    }
-
-    @Test
-    public void givenFleetWithTwoShipsWhenGetAllFleetPositionsThenReturnAllPositions() {
-        CustomFleet fleet = new CustomFleet();
-        fleet.addShip(new ShipModel(Arrays.asList(0)));
-        fleet.addShip(new ShipModel(Arrays.asList(1, 2)));
-
-        assertThat(fleet.getFleetPositions()).containsExactly(0, 1, 2);
-    }
-
-    private ShipModel generateOneMastShip() {
-        Random random = new Random();
-        Integer[] positions = {random.nextInt(11)};
-
-        return new ShipModel(new ArrayList<>(Arrays.asList(positions)));
-    }
-
-    private ShipModel generateTwoMastShip() {
-        Random random = new Random();
-        Integer[] positions = {random.nextInt(11), random.nextInt(11)};
-
-        return new ShipModel(new ArrayList<>(Arrays.asList(positions)));
+        assertThat(actualHits).contains(hitPosition);
     }
 }
--- server/src/test/java/fleet/HardcodedFleet.java
+++ server/src/test/java/fleet/HardcodedFleet.java
@@ -4,16 +4,17 @@ import model.ShipModel;
 
 import java.util.*;
 
+/**
+ * This class is used for HitChecker and Referee
+ */
 public class HardcodedFleet implements Fleet {
 
     private static final Integer[] HARDCODED_POSITIONS = {0, 1, 2, 3, 17, 18, 19, 34, 40, 41, 44, 47, 54, 57, 61, 77, 78, 82, 85, 99};
     private final List<Integer> fleetPositions;
     private List<Integer> hitFields;
-    private final List<ShipModel> ships;
 
 
     public HardcodedFleet() {
-        ships = new ArrayList<>();
         fleetPositions = new ArrayList<>();
         hitFields = new ArrayList<>();
         fleetPositions.addAll(Arrays.asList(HARDCODED_POSITIONS));
@@ -33,15 +34,4 @@ public class HardcodedFleet implements Fleet {
     public List<Integer> getHitFields() {
         return hitFields;
     }
-
-    @Override
-    public int getSize() {
-        return ships.size();
-    }
-
-    @Override
-    public List<ShipModel> getShips() {
-        return new ArrayList<>();
-    }
-
 }
