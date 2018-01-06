157d7db4a63dee515cac733568ce6e6afaec92b1
bartek <bartek6824@gmail.com>
2018-01-06 19:45:01 +0100

    adding properties file

--- server/src/main/java/communication/Server.java
+++ server/src/main/java/communication/Server.java
@@ -1,10 +1,11 @@
 package communication;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  * It starts listening for connections on the specified port.
@@ -13,18 +14,26 @@ import java.util.logging.Logger;
  */
 public class Server {
 
-  private static final int PORT = 5000;
   private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+  private static final String fileName ="config.properties";
 
   public void runServer() {
     LOGGER.info("Server running!");
     ConnectionHandler connectionHandler = new ConnectionHandler();
     try {
-      ServerSocket serverSocket = new ServerSocket(PORT);
+        int portNumber = setUpServerConfig();
+      ServerSocket serverSocket = new ServerSocket(portNumber);
       connectionHandler.acceptConnections(serverSocket);
     } catch (IOException e) {
       LOGGER.log(Level.SEVERE, e.getMessage());
     }
+  }
+
+  private int setUpServerConfig() throws IOException{
+      Properties properties = new Properties();
+      InputStream config = ClassLoader.getSystemResourceAsStream(fileName);
+      properties.load(config);
+      return Integer.parseInt(properties.getProperty("portNumber"));
   }
 }
 
