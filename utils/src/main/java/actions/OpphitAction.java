588b8ff1be9fa8c828396f54ddc811a12feeedcb
Bartosz Pieczara <bartosz_pieczara@epam.com>
2018-01-08 17:28:12 +0100

    added opphitaction class

---
+++ utils/src/main/java/actions/OpphitAction.java
@@ -0,0 +1,11 @@
+package actions;
+
+import responses.PerformAction;
+import responses.SuperiorMessage;
+
+public class OpphitAction implements PerformAction {
+
+  @Override
+  public void act(SuperiorMessage message) {
+  }
+}
