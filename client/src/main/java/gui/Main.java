83b7ed154a67d1bacc4a5dc6cf037246a61a245d
Bartosz Pieczara <bartosz_pieczara@epam.com>
2018-01-17 12:53:00 +0100

    adds IP validation

--- client/src/main/java/gui/Main.java
+++ client/src/main/java/gui/Main.java
@@ -2,14 +2,50 @@ package gui;
 
 import gui.starting.StartBoard;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * It starts the gui.
  *
  * @version 1.5
  */
 class Main {
+  private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+  private static final String SERVER_CONFIG_FILE = "config.properties";
 
   public static void main(String[] args) {
-    StartBoard.run(args);
+    try {
+      if(isIPvalid()){
+        StartBoard.run(args);
+      } else {
+        LOGGER.log(Level.INFO,"Closing the game as the IP was not valid");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
+
+  private static boolean isIPvalid() throws IOException {
+
+    String IPADDRESS_PATTERN = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+    Properties properties = new Properties();
+    InputStream config = ClassLoader.getSystemResourceAsStream(SERVER_CONFIG_FILE);
+    properties.load(config);
+    String retrievedIP = properties.getProperty("IP");
+
+    Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+    Matcher matcher = pattern.matcher(retrievedIP);
+    return matcher.matches();
+  }
+
 }
