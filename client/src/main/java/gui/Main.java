473dbcaada393b35ec246a638a8ae9b3992d150c
patiem <pp.mikulska@gmail.com>
2018-01-18 11:47:05 +0100

    Add parameter reading for starting app and change images on gui

--- client/src/main/java/gui/Main.java
+++ client/src/main/java/gui/Main.java
@@ -2,16 +2,15 @@ package gui;
 
 import gui.starting.StartBoard;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- * It starts the gui.
+ * Configure application.
  *
  * @version 1.5
  */
@@ -19,43 +18,35 @@ class Main {
 
   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
   private static final String SERVER_CONFIG_FILE = "config.properties";
+  private static final String PATH_TO_PROPS = "utils/src/main/resources/";
 
-  public static void main(String[] args) {
+
+  public static void main(String[] args) throws IOException {
+
+    if (args.length > 0) {
+      setupNewIpForHost(args[0]);
+    }
     try {
-      if (isServerConfigValid()) {
+      if (HostValidator.isServerConfigValid(PATH_TO_PROPS + SERVER_CONFIG_FILE)) {
         StartBoard.run(args);
-      } else {
-        LOGGER.log(Level.INFO, "Closing the game as the IP was not valid or port is invalid");
       }
     } catch (IOException e) {
       LOGGER.log(Level.SEVERE, e.getMessage());
     }
   }
 
-  private static boolean isServerConfigValid() throws IOException {
-
-    String ipPattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
-        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
-        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
-        + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+  public static void setupNewIpForHost(String newIpForHost) throws IOException {
 
     Properties properties = new Properties();
     InputStream config = ClassLoader.getSystemResourceAsStream(SERVER_CONFIG_FILE);
+
     properties.load(config);
-    String retrievedIP = properties.getProperty("IP");
-    int retrievedPort = Integer.parseInt(properties.getProperty("portNumber"));
+    properties.setProperty("IP", newIpForHost);
 
-    Pattern pattern = Pattern.compile(ipPattern);
-    Matcher matcher = pattern.matcher(retrievedIP);
+    FileWriter fileWriter = new FileWriter(PATH_TO_PROPS + SERVER_CONFIG_FILE);
+    properties.store(fileWriter, null);
+    fileWriter.flush();
+    fileWriter.close();
 
-    Pattern patternLocal = Pattern.compile("localhost", Pattern.CASE_INSENSITIVE);
-    Matcher matcherLocal = patternLocal.matcher(retrievedIP);
-
-    return (matcher.matches() || matcherLocal.matches()) && isPortValid(retrievedPort);
   }
-
-  private static boolean isPortValid(int portNumber) {
-    return portNumber > 1023 && portNumber < 65537;
-  }
-
 }
