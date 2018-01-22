f61b641e6e1e2ff0827019a730906da18064cb46
Emilia <emciastek@gmail.com>
2018-01-22 10:42:57 +0100

    Delete unused fields in GameRunnerTest

--- server/src/test/java/engine/GameRunnerTest.java
+++ server/src/test/java/engine/GameRunnerTest.java
@@ -1,12 +1,9 @@
 package engine;
 
-import communication.PlayerRegistry;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -14,58 +11,16 @@ import static org.mockito.Mockito.when;
 
 public class GameRunnerTest {
 
-  private GameState activeGameMock;
-  private FinishedGame finishedGameMock;
-  private PlayerRegistry playerRegistry;
-  private GameRunner gameRunner;
-
-  @BeforeMethod
-  public void setUp() {
-    activeGameMock = mock(GameState.class);
-    playerRegistry = mock(PlayerRegistry.class);
-    gameRunner = new GameRunner(activeGameMock);
-    finishedGameMock = mock(FinishedGame.class);
-  }
-
-//  @Test
-//  public void whenTheRunMethodIsCalledLaunchTheGame() throws Exception {
-//    // Arrange
-//    when(activeGameMock.run()).thenReturn(finishedGameMock);
-//    // Act
-//    gameRunner.runGame();
-//    // Assert
-//    verify(activeGameMock, times(1)).run();
-//  }
-//
-//  @Test
-//  public void whenTheRunMethodIsCalledThenInvokeTheSendResponseMethod() throws Exception {
-//    // Arrange
-//    when(activeGameMock.run()).thenReturn(finishedGameMock);
-//    // Act
-//    gameRunner.runGame();
-//    // Assert
-//    verify(finishedGameMock, times(1)).sendFinalResponse();
-//  }
-//
-//  @Test
-//  public void whenTheRunMethodIsCalledThenInvokeTheSendResponseMethodWithArguments() throws Exception {
-//    // Arrange
-//    PlayerClient playerClient = mock(PlayerClient.class);
-//    when(playerRegistry.getCurrentPlayer()).thenReturn(playerClient);
-//    when(activeGameMock.run()).thenReturn(finishedGameMock);
-//    // Act
-//    gameRunner.runGame();
-//    // Assert
-//    verify(activeGameMock, times(1)).sendResponse(any(PlayResponse.class),any(PlayerClient.class));
-//  }
-
   @Test
   public void shouldInvokeRunMethodOnProvidedState() throws IOException {
-    when(activeGameMock.isGameRunning()).thenReturn(false);
-    when(activeGameMock.run()).thenReturn(activeGameMock);
+    GameState mockedGameState = mock(GameState.class);
+    GameRunner gameRunner = new GameRunner(mockedGameState);
+
+    when(mockedGameState.isGameRunning()).thenReturn(false);
+    when(mockedGameState.run()).thenReturn(mockedGameState);
 
     gameRunner.runGame();
 
-    verify(activeGameMock, times(1)).run();
+    verify(mockedGameState, times(1)).run();
   }
 }
