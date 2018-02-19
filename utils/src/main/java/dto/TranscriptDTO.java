86a1cf7b99377d2a7a76d2cc228c5684b278cdda
rivalez <marektabor1@gmail.com>
2018-02-19 10:07:07 +0100

    send transcript dto. but encapsulation is broken

--- utils/src/main/java/dto/TranscriptDTO.java
+++ utils/src/main/java/dto/TranscriptDTO.java
@@ -1,5 +1,7 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.Immutable;
 
 import java.io.Serializable;
@@ -8,10 +10,14 @@ import java.util.Date;
 @Immutable
 public class TranscriptDTO implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @JsonProperty("message")
     private String message;
+    @JsonProperty("date")
     private Date date;
 
-    private TranscriptDTO(String message, Date date) {
+    @JsonCreator
+    public TranscriptDTO(@JsonProperty("message") String message, @JsonProperty("date") Date date) {
         this.message = message;
         this.date = date;
     }
